f6a22409f6238962e175d1bce9e73fba34001361
Sam Halliday <sam.halliday@gmail.com>
2020-02-28 11:14:08 +0000

    downgrade FIXME to TODO

--- library/HsInspect/Index.hs
+++ library/HsInspect/Index.hs
@@ -128,7 +128,7 @@ hiToSymbols
   -> m (Maybe (GHC.Module, [(Maybe GHC.Module, GHC.TcTyThing)]))
 hiToSymbols exposed hi = (join <$>) <$> withHi hi $ \iface -> do
   let m = mi_module iface
-  -- FIXME we should include all modules from inplace packages, otherwise the
+  -- TODO we should include all modules from inplace packages, otherwise the
   -- user is unable to jump-to-definition within the same multi-package project.
   if not $ Set.member (GHC.moduleName m) exposed
     then pure Nothing
@@ -186,7 +186,7 @@ data PackageEntries = PackageEntries (Maybe SourcePackageId) Bool [ModuleEntries
 -- srcid is Nothing if it matches the re-export location
 data Exported = Exported (Maybe SourcePackageId) GHC.ModuleName
 
--- FIXME Exported should follow re-exports until they reach the original symbol.
+-- TODO Exported should follow re-exports until they reach the original symbol.
 -- Otherwise editors have to do this.
 mkExported :: GHC.DynFlags -> UnitId -> Module -> Exported
 mkExported dflags unitid m =
